475f1bea46e6cd6dc77c0c50975fc47c3c956af1
Guilherme <guil_stay@hotmail.com>
2018-08-28 19:50:35 -0300

    correcao

--- Exercicio02 2018_08_21/atv09.py
+++ Exercicio02 2018_08_21/atv09.py
@@ -22,25 +22,31 @@
 # 9.8.234.5
 # 192.168.0.256
 
-import re
-def regex(pattern, string):
-	capt = re.match(pattern, string)
-	if bool(capt):
-		return capt.group()
-	return False
+import socket
 
-ip_v = ''
-ip_i = ''
-list = {'0.25.33.19', '127.0.1.1','127.0.0.2','127.0.0.1','192.0.0.256','0.168.1.36','1.1.0.99','200.135.80.9','192.168.1.1','8.35.67.74','257.32.4.5','85.345.1.2','1.2.3.4','9.8.234.5','192.168.0.256'}
-for ip in list:
-	if ip == '0.0.0.0':
-		ip_v = ip_v + '\n' + ip
-	elif regex('^(([1-9]\.|[1-9][0-9]\.|1[0-9][0-9]\.|2[0-4][0-9]\.|25[0-5]\.)([0-9]\.|[1-9][0-9]\.|1[0-9][0-9]\.|2[0-4][0-9]\.|25[0-5]\.)([0-9]\.|[1-9][0-9]\.|1[0-9][0-9]\.|2[0-4][0-9]\.|25[0-5]\.)([0-9]\.|[1-9][0-9]\.|1[0-9][0-9]\.|2[0-4][0-9]\.|25[0-5]\.))$', ip + '.'):
-		ip_v = ip_v + '\n' + ip
-	else:
-		ip_i = ip_i + '\n' + ip
+ipsValidos = []
+ipsInvalidos = []
 
-print('[Endereços válidos:]')
-print(ip_v)
-print('[Endereços inválidos:]')
-print(ip_i)
+ips = open("ips.txt")
+for ip in ips:
+    try:
+        socket.inet_aton(ip)
+        # Validos
+        ipsValidos.append(ip)
+    except socket.error:
+        # Invalidos
+        ipsInvalidos.append(ip)
+        
+ips.close()
+
+saida = open("ipsvalidados.txt", "w+")
+
+saida.write("[Endereços válidos:]\n")
+for ip in ipsValidos:
+    saida.write(ip)
+    
+saida.write("[Endereços inválidos:]\n")
+for ip in ipsInvalidos:
+    saida.write(ip)
+    
+saida.close()
