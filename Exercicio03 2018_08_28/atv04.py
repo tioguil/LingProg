a01d9f8f996c6d62501e9256f0decb394636d2e7
Guilherme <guil_stay@hotmail.com>
2018-09-04 20:32:26 -0300

    last commit primary exer

--- Exercicio03 2018_08_28/atv04.py
+++ Exercicio03 2018_08_28/atv04.py
@@ -1,14 +1,19 @@
 # 4. Faça um Programa que leia três números e mostre-os em ordem
 # decrescente.
 
-list = []
+numero_1 = int(input("Digite o primeiro número: "))
+numero_2 = int(input("Digite o segundo número: "))
+numero_3 = int(input("Digite o terceiro número: "))
 
-list.append(int(input("entre com primeiro valor")))
-list.append(int(input("entre com segundo valor")))
-list.append(int(input("entre com terceiro valor")))
-
-list.sort()
-list = reversed(list)
-
-for a in list:
-    print(a)
+if numero_2 > numero_1 and numero_1 > numero_3:
+    print("%s - %s - %s" %(numero_2, numero_1, numero_3))
+elif numero_2 > numero_1 and numero_3 > numero_2:
+    print("%s - %s - %s" %(numero_3, numero_2, numero_1))
+elif numero_2 > numero_3 and numero_3 > numero_1:
+    print("%s - %s - %s" %(numero_2, numero_3, numero_1))
+elif numero_1 > numero_2 and numero_3 > numero_1:
+    print("%s - %s - %s" %(numero_3, numero_1, numero_2))
+elif numero_1 > numero_2 and numero_3 > numero_2:
+    print("%s - %s - %s" %(numero_1, numero_3, numero_2))
+else:
+    print("%s - %s - %s" %(numero_1, numero_2, numero_3))
